7454e01cea9fbcd4817afe43e921a7b9762409cd
Raymond Oluoch <rasholuoch@gmail.com>
2024-05-23 12:55:17 +0300

    updated documentation

---
+++ .venv/Lib/site-packages/in_toto/__init__.py
@@ -0,0 +1,11 @@
+# Copyright New York University and the in-toto contributors
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Configure base logger for in_toto (see in_toto.log for details).
+
+"""
+import in_toto.log
+
+# in-toto version
+__version__ = "3.0.0"
